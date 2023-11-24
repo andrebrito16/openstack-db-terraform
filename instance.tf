6537a8eaba791e89ac8c8132ddb46eca763321f5
Andre Brito <felipe.brito016@gmail.com>
2023-11-24 17:51:22 -0300

    Added init script

--- instance.tf
+++ instance.tf
@@ -5,6 +5,8 @@ resource "openstack_compute_instance_v2" "instancia" {
   key_pair        = "mass_key"
   security_groups = ["default"]
 
+  user_data = file("${path.module}/scripts/init_db.sh")
+
   network {
     name = "network_1"
   }
